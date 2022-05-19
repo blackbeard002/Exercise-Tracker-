858ffa55b80a38668d3f0ede8ac967eb8509c3bd
Deepu <deepuharry02@gmail.com>
2022-05-19 17:55:37 +0530

    Database connection setup:v1

---
+++ backend/routes/users.js
@@ -0,0 +1,22 @@
+const router=require('express').Router();
+let User = require('../models/user.model');
+
+//first end point that handles http GET requests
+router.route('/').get((req,res) => {
+    User.find() //finds all users from MongoDB 
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: '+err));
+});
+
+//second end point that handles http post requests
+router.route('/add').post((req,res) => {
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: '+err));
+});
+
+module.exports=router; 
